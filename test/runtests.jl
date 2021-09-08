793dacedde5bed9f8ac02d620235977c485a4180
eduardosalaz <eduardosalaz@outlook.com>
2021-09-07 20:37:50 -0500

    more tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,5 +5,6 @@ using Test
 @testset "Ergast.jl" begin
     @testset "Drivers' data" begin
         @test nrow(GetDrivers(cons="ferrari", year="2020")) == 2
+        @test GetDrivers(cons="mclaren", circuit="istanbul", fastest="1")[!,1][1] == "norris"
     end
 end
